765880d78fc25924f5fe941569a559e03ec95af9
mlab817 <mlab817@gmail.com>
2020-12-14 18:38:13 +0800

    made user model retrievable from config

--- src/GraphQL/Mutations/AssignRole.php
+++ src/GraphQL/Mutations/AssignRole.php
@@ -2,7 +2,6 @@
 
 namespace Mlab817\LighthouseGraphQLPermission\GraphQL\Mutations;
 
-use App\User;
 use Mlab817\LighthouseGraphQLPermission\Traits\DefaultGuard;
 use Spatie\Permission\Models\Role;
 
@@ -17,12 +16,19 @@ class AssignRole
      */
     public function __invoke($_, array $args)
     {
-        $user = User::findOrFail($args['user_id']);
+        $userModel = $this->getUserClass();
+
+        $user = $userModel::findOrFail($args['user_id']);
 
         $role = Role::findByName($args['role'], $this->guard);
 
         $user->assignRole($role);
 
         return $user;
+    }
+
+    public function getUserClass(): string
+    {
+        return config('auth.providers.users.model');
     }
 }
